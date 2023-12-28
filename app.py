582a256fafbd0c5436401710740077ca4d228e1a
aavetis <albert.avetisian@gmail.com>
2023-12-28 15:35:16 -0500

    change input

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import gradio as gr
 from uform import gen_model
 from PIL import Image
-import requests
 
 # Load the model and processor
 model = gen_model.VLMForCausalLM.from_pretrained("unum-cloud/uform-gen")
 processor = gen_model.VLMProcessor.from_pretrained("unum-cloud/uform-gen")
-
 
 def generate_caption(image, prompt):
     # Process the image and the prompt
@@ -18,18 +16,14 @@ def generate_caption(image, prompt):
 
     return caption
 
-
 # Load the demo image
 demo_image = Image.open("jungle-glass.png")
 
 # Define the Gradio interface
 iface = gr.Interface(
     fn=generate_caption,
-    inputs=[
-        gr.inputs.Image(type="pil", label="Upload Image", default=demo_image),
-        gr.inputs.Textbox(label="Prompt"),
-    ],
-    outputs=gr.outputs.Textbox(label="Generated Caption"),
+    inputs=[gr.Image(type="pil", label="Upload Image", default=demo_image), gr.Textbox(label="Prompt")],
+    outputs=gr.Textbox(label="Generated Caption"),
 )
 
 # Launch the interface
